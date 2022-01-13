30e44b4a40f24471dbc8136d4ebafa042eee8a56
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2022-01-13 12:48:14 -0800

    Catch a couple known Ghostscript problems.

--- src/PdfThumbnailsTest/Software/Ghostscript.php
+++ src/PdfThumbnailsTest/Software/Ghostscript.php
@@ -7,14 +7,26 @@ class Ghostscript extends SoftwareAbstract {
   public $type = 'server';
   public $name = 'Ghostscript';
 
-  public $errorMessage = 'Your server is missing software';
+  public $errorMessage = 'Your server is missing software.';
   public $actionMessage = 'Contact your host to install the missing software below';
 
   protected function runTest() {
-    if(`which gs`) {
-      return true;
+    $imagick = new Imagick();
+    if(empty($imagick->queryFormats('PDF'))) {
+      $this->actionMessage = 'ImageMagick is not reporting PDFs as an available format. This likely means it cannot find Ghostscript.'
+        return false;
     }
-    return false;
+    try {
+      // bundle a sample PDF
+      $imagick = new Imagick('sample.pdf');
+    } catch(\ImagickException $e) {
+      $this->errorMessage = 'There is a configuration error.';
+      if(/*'attempt to perform an operation not allowed by the security policy'*/) {
+        $this->actionMessage = 'Ghostscript is available but the PDF security policy for ImageMagick disables reading PDFs. See https://imagemagick.org/script/security-policy.php for more information.';
+      } else {
+        $this->actionMessage = 'There is a problem with ImageMagick\'s Ghostscript delegate.';
+      }
+    }
   }
 
 }
